949dc567fe41e8aa8e02fced6978439f0bc41aab
kx-huang <Huang.KX@sjtu.edu.cn>
2022-02-15 16:21:34 -0600

    fix: format

--- io.h
+++ io.h
@@ -6,19 +6,20 @@
 #include "process.h"
 
 #define RESET "\x1B[1;0m"
-#define BLK   "\x1B[1;30m"
-#define RED   "\x1B[1;31m"
-#define GRN   "\x1B[1;32m"
-#define YEL   "\x1B[1;33m"
-#define BLU   "\x1B[1;34m"
-#define MAG   "\x1B[1;35m"
-#define CYN   "\x1B[1;36m"
-#define WHT   "\x1B[1;37m"
-#define ORGB  "\x1B[43m"
-#define BLUB  "\x1B[44m"
-#define PURB  "\x1B[45m"
+#define BLK "\x1B[1;30m"
+#define RED "\x1B[1;31m"
+#define GRN "\x1B[1;32m"
+#define YEL "\x1B[1;33m"
+#define BLU "\x1B[1;34m"
+#define MAG "\x1B[1;35m"
+#define CYN "\x1B[1;36m"
+#define WHT "\x1B[1;37m"
+#define ORGB "\x1B[43m"
+#define BLUB "\x1B[44m"
+#define PURB "\x1B[45m"
 
 void mumsh_read_cmds();
+void prompt_prefix();
 void prompt_path();
 void prompt_mumsh();
 void read_dangling_cmds(char *buffer);
