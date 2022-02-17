7b60fe459018c45ae2f4af33f9d8143f0a6d9d61
kx-huang <Huang.KX@sjtu.edu.cn>
2022-02-17 00:18:22 -0600

    update: function longest fit

--- hinter.c
+++ hinter.c
@@ -1,50 +1,24 @@
 #include "hinter.h"
 
-#define cursor_forward(x) printf("\033[%dC", (x))
-#define cursor_backward(x) printf("\033[%DD", (x))
-#define cursor_upward(x) printf("\033[%DA", (x))
-
-#define KEY_ESCAPE 0x001b
-#define KEY_DELETE 0x007f
-#define KEY_TAB 0x0009
-#define KEY_ENTER 0x000a
-#define KEY_UP 0x0105
-#define KEY_DOWN 0x0106
-#define KEY_RIGHT 0x0107
-#define KEY_LEFT 0x0108
-
-static struct termios term, oterm;
-
-static int getch(void);
-static int kbhit(void);
-static int kbesc(void);
-static int kbget(void);
-
 size_t len;
 size_t pos;
 size_t num_hint;
 size_t width_clean;
 size_t offset_prefix;
 char token[BUFFER_SIZE];
-char puzzle[TOKEN_SIZE];
+char puzzle[BUFFER_SIZE];
 char path[TOKEN_SIZE];
+char fit[TOKEN_SIZE];
 char hint[BUFFER_SIZE][TOKEN_SIZE] = {{0}};
 
-static int getch(void) {
-  int c = 0;
-  tcgetattr(0, &oterm);
-  memcpy(&term, &oterm, sizeof(term));
-  term.c_lflag &= (size_t) ~(ICANON | ECHO);
-  term.c_cc[VMIN] = 1;
-  term.c_cc[VTIME] = 0;
-  tcsetattr(0, TCSANOW, &term);
-  c = getchar();
-  clearerr(stdin);
-  tcsetattr(0, TCSANOW, &oterm);
-  return c;
+// capture keyboard event
+int keyboard_get(void) {
+  int c = keyboard_char();
+  return (c == KEY_ESCAPE) ? keyboard_arrow() : c;
 }
 
-static int kbhit(void) {
+// capture keyboard hit
+int keyboard_hit(void) {
   int c = 0;
   tcgetattr(0, &oterm);
   memcpy(&term, &oterm, sizeof(term));
@@ -59,11 +33,27 @@ static int kbhit(void) {
   return (c != -1 ? 1 : 0);
 }
 
-static int kbesc(void) {
-  if (!kbhit()) return KEY_ESCAPE;
-  int c = getch();
+// capture keyboard character
+int keyboard_char(void) {
+  int c = 0;
+  tcgetattr(0, &oterm);
+  memcpy(&term, &oterm, sizeof(term));
+  term.c_lflag &= (size_t) ~(ICANON | ECHO);
+  term.c_cc[VMIN] = 1;
+  term.c_cc[VTIME] = 0;
+  tcsetattr(0, TCSANOW, &term);
+  c = getchar();
+  clearerr(stdin);
+  tcsetattr(0, TCSANOW, &oterm);
+  return c;
+}
+
+// capture keyboardd arrow
+int keyboard_arrow(void) {
+  if (!keyboard_hit()) return KEY_ESCAPE;
+  int c = keyboard_char();
   if (c == '[') {
-    switch (getch()) {
+    switch (keyboard_char()) {
       case 'A':
         c = KEY_UP;
         break;
@@ -84,22 +74,81 @@ static int kbesc(void) {
     c = 0;
   }
   if (c == 0)
-    while (kbhit()) getch();
+    while (keyboard_hit()) keyboard_char();
   return c;
 }
 
-static int kbget(void) {
-  int c = getch();
-  return (c == KEY_ESCAPE) ? kbesc() : c;
-}
-
-// move cursor to user buffer
-void _calibrate_cursor(size_t pos_now) {
-  cursor_upward((int)num_hint);
-  if (pos_now < pos + offset_prefix)
-    cursor_forward((int)(pos + offset_prefix - pos_now));
-  else if (pos_now > pos + offset_prefix)
-    cursor_backward((int)(pos_now - pos - offset_prefix));
+// hinter main capture/calculate/print loop
+void mumsh_hinter(char *buffer) {
+  strcpy(path, "./");
+  // main loop
+  while (1) {
+    // get keyboard input
+    int c = keyboard_get();
+    // enter key
+    if (c == KEY_ENTER) {
+      clean_hint();
+      buffer[len] = '\n';
+      printf("\n");
+      break;
+    }
+    // escape key
+    if (c == KEY_ESCAPE) continue;
+    // tab key
+    if (c == KEY_TAB) {
+      clean_hint();
+      int type = hint_type(buffer);
+      // cursor after space or none: all hint
+      if (type == 1) {
+        strcpy(puzzle, "");
+        strcpy(path, "./");
+      }
+      // cursor after token: matched hint
+      if (type == 2) {
+        find_token(buffer, " <>|");
+        create_puzzle();
+        // if token is a path, print exclude "." and ".."
+        type = strlen(puzzle) ? 2 : 1;
+      }
+      find_match(type);
+      // auto complete if only one match
+      if (num_hint >= 1) {
+        longest_fit();
+        auto_complete(buffer);
+      }
+      if (num_hint > 1) print_hint();
+      continue;
+    }
+    // up and down key
+    if (c == KEY_UP | c == KEY_DOWN) {
+      // TODO: show history
+      continue;
+    }
+    // left arrow key
+    if (c == KEY_LEFT) {
+      if (pos > 0) {
+        cursor_backward(1);
+        pos--;
+      }
+      continue;
+    }
+    // right arrow key
+    if (c == KEY_RIGHT) {
+      if (pos < len) {
+        cursor_forward(1);
+        pos++;
+      }
+      continue;
+    }
+    // delete key
+    if (c == KEY_DELETE) {
+      delete_char(buffer);
+      continue;
+    }
+    // ordinary buffer
+    write_char(c, buffer);
+  }
+  // debug_hinter(buffer);
 }
 
 // clean printed matched fileneams
@@ -112,7 +161,26 @@ void clean_hint() {
   }
 }
 
+// determine hint type
+// input: buffer
+// hint type: 0 - no hint
+//            1 - full hint (without "." and "..")
+//            2 - matched hint (include "." and "..")
+int hint_type(char buffer[BUFFER_SIZE]) {
+  // cursor at leftmost
+  if (pos == 0 && len == 0) return 1;
+  if (pos == 0 && len > 0) return buffer[pos] == ' ' ? 1 : 0;
+  // cursor at rightmost
+  if (pos == len) return buffer[pos - 1] == ' ' ? 1 : 2;
+  //  cursor in middle
+  if (buffer[pos - 1] == ' ' && buffer[pos] == ' ') return 1;
+  if (buffer[pos - 1] != ' ' && buffer[pos] == ' ') return 2;
+  return 0;
+}
+
 // find current token
+// input: buffer[], delimiter[]
+// update: token[]
 void find_token(char buffer[BUFFER_SIZE], char delimiter[8]) {
   char *tmp = NULL;
   char input_dup[BUFFER_SIZE];
@@ -127,6 +195,8 @@ void find_token(char buffer[BUFFER_SIZE], char delimiter[8]) {
 }
 
 // separate token into path and puzzle
+// input: token[]
+// output: path[], puzzle[]
 void create_puzzle() {
   // find last "/"
   int pos_slash = -1;
@@ -152,8 +222,10 @@ void create_puzzle() {
   puzzle[len_token - (size_t)pos_slash - 1] = 0;
 }
 
-// return number of matched files
-void solve_puzzle(int type) {
+// find all matched files
+// input: hint type (0 - no hint, 1 - full hint, 2 - matched hint)
+// update: hint[][], num_hint
+void find_match(int type) {
   num_hint = 0;
   struct dirent *dir;
   DIR *d = opendir(path);
@@ -190,7 +262,46 @@ void solve_puzzle(int type) {
   }
 }
 
-// return position of the cursor after printing
+// find the longest fit in match
+// input: hint[][]
+// update: fit[]
+void longest_fit() {
+  memset(fit, 0, TOKEN_SIZE);
+  if (num_hint <= 0) return;
+  if (num_hint == 1) strcpy(fit, hint[0]);
+  for (size_t j = 0; hint[0][j]; j++) {
+    char c = hint[0][j];
+    int same = 1;
+    for (size_t i = 1; i < num_hint; i++)
+      if (hint[i][j] != c) {
+        same = 0;
+        break;
+      }
+    if (!same) break;
+    fit[j] = c;
+  }
+}
+
+// auto complete the longest fit of match
+// matched part
+// input: fit[], puzzle[]
+// update: buffer[], pos, len
+void auto_complete(char buffer[BUFFER_SIZE]) {
+  if (strlen(fit)) {
+    size_t offset = len - pos;
+    size_t len_puzzle = strlen(puzzle);
+    size_t len_add = strlen(fit) - len_puzzle;
+    len += len_add;
+    for (size_t i = len - 1; i >= pos + len_add; i--)
+      buffer[i] = buffer[i - len_add];
+    for (size_t i = 0; i < len_add; i++) buffer[pos + i] = fit[len_puzzle + i];
+    for (size_t i = pos; i < len; i++) putchar(buffer[i]);
+    pos += len_add;
+    if (offset > 0) cursor_backward((int)(offset));
+  }
+}
+
+// print matched filenames
 void print_hint() {
   if (num_hint) {
     for (size_t i = 0; i < num_hint; i++) printf("\n%s", hint[i]);
@@ -198,41 +309,8 @@ void print_hint() {
   }
 }
 
-// auto complete the matched part of filename
-void auto_complete(char buffer[BUFFER_SIZE]) {
-  // find the maximum match
-  char match[TOKEN_SIZE] = {0};
-  if (num_hint > 1) {
-    for (size_t j = 0; hint[0][j]; j++) {
-      char c = hint[0][j];
-      int same = 1;
-      for (size_t i = 1; i < num_hint; i++)
-        if (hint[i][j] != c) {
-          same = 0;
-          break;
-        }
-      if (!same) break;
-      match[j] = c;
-    }
-  }
-  if (num_hint == 1) strcpy(match, hint[0]);
-  // plug in matched part
-  if (strlen(match)) {
-    size_t offset = len - pos;
-    size_t len_puzzle = strlen(puzzle);
-    size_t len_add = strlen(match) - len_puzzle;
-    len += len_add;
-    for (size_t i = len - 1; i >= pos + len_add; i--)
-      buffer[i] = buffer[i - len_add];
-    for (size_t i = 0; i < len_add; i++)
-      buffer[pos + i] = match[len_puzzle + i];
-    for (size_t i = pos; i < len; i++) putchar(buffer[i]);
-    pos += len_add;
-    if (offset > 0) cursor_backward((int)(offset));
-  }
-}
-
-//  input one char
+// input one char
+// update: buffer, pos, len
 void write_char(int c, char buffer[BUFFER_SIZE]) {
   putchar(c);
   for (size_t i = ++len; i > pos; i--) buffer[i] = buffer[i - 1];
@@ -242,6 +320,7 @@ void write_char(int c, char buffer[BUFFER_SIZE]) {
 }
 
 // delete one char
+// update: buffer, pos, len
 void delete_char(char buffer[BUFFER_SIZE]) {
   if (pos) {
     cursor_backward(1);
@@ -254,97 +333,17 @@ void delete_char(char buffer[BUFFER_SIZE]) {
   }
 }
 
-// hint type: 0 - no hint
-//            1 - full hint (without "." and "..")
-//            2 - matched hint (include "." and "..")
-int hint_type(char buffer[BUFFER_SIZE]) {
-  // cursor at leftmost
-  if (pos == 0 && len == 0) return 1;
-  if (pos == 0 && len > 0) return buffer[pos] == ' ' ? 1 : 0;
-  // cursor at rightmost
-  if (pos == len) return buffer[pos - 1] == ' ' ? 1 : 2;
-  //  cursor in middle
-  if (buffer[pos - 1] == ' ' && buffer[pos] == ' ') return 1;
-  if (buffer[pos - 1] != ' ' && buffer[pos] == ' ') return 2;
-  return 0;
+// move cursor to user input
+void _calibrate_cursor(size_t pos_now) {
+  cursor_upward((int)num_hint);
+  if (pos_now < pos + offset_prefix)
+    cursor_forward((int)(pos + offset_prefix - pos_now));
+  else if (pos_now > pos + offset_prefix)
+    cursor_backward((int)(pos_now - pos - offset_prefix));
 }
 
-void hint_interface(char *buffer) {
-  strcpy(path, "./");
-  // main loop
-  while (1) {
-    // get keyboard input
-    int c = kbget();
-    // enter key
-    if (c == KEY_ENTER) {
-      clean_hint();
-      buffer[len] = '\n';
-      printf("\n");
-      break;
-    }
-    // escape key
-    if (c == KEY_ESCAPE) continue;
-    // tab key
-    if (c == KEY_TAB) {
-      // reset variable
-      clean_hint();
-      int type = hint_type(buffer);
-      // all hint: after space or none
-      if (type == 1) {
-        strcpy(puzzle, "");
-        strcpy(path, "./");
-        solve_puzzle(type);
-        // only one match: auto complete
-        if (num_hint >= 1) auto_complete(buffer);
-        if (num_hint > 1) print_hint();
-        continue;
-      }
-      // matched hint: after token
-      if (type == 2) {
-        find_token(buffer, " <>|");
-        create_puzzle();
-        // if token is a path, print exclude "." and ".."
-        type = strlen(puzzle) ? 2 : 1;
-        solve_puzzle(type);
-        // auto complete if only one found
-        if (num_hint >= 1) auto_complete(buffer);
-        if (num_hint > 1) print_hint();
-      }
-      continue;
-    }
-    // up and down key
-    if (c == KEY_UP | c == KEY_DOWN) {
-      // TODO: show history
-      continue;
-    }
-    // left arrow key
-    if (c == KEY_LEFT) {
-      if (pos > 0) {
-        cursor_backward(1);
-        pos--;
-      }
-      continue;
-    }
-    // right arrow key
-    if (c == KEY_RIGHT) {
-      if (pos < len) {
-        cursor_forward(1);
-        pos++;
-      }
-      continue;
-    }
-    // delete key
-    if (c == KEY_DELETE) {
-      delete_char(buffer);
-      continue;
-    }
-    // ordinary buffer
-    write_char(c, buffer);
-  }
-  // debug_hint(buffer);
-}
-
-void debug_hint(char *buffer) {
+// debug hinter
+void debug_hinter(char *buffer) {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
   printf("------ Debug ------\n");
